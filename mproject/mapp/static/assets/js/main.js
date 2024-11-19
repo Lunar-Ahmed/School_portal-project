8e829e876b10418862f249ea1b67747849e33f26
Ahmed <lunitmohammed697@gmail.com>
2024-11-19 01:32:13 +0100

    updates

--- mproject/mapp/static/assets/js/main.js
+++ mproject/mapp/static/assets/js/main.js
@@ -260,3 +260,12 @@
   new PureCounter();
 
 })()
+
+
+document.getElementById('import-btn').addEventListener('click', function() {
+  document.getElementById('file-input').click();
+});
+
+document.getElementById('file-input').addEventListener('change', function() {
+  console.log(this.files[0]); // Log the selected file for demonstration
+});
