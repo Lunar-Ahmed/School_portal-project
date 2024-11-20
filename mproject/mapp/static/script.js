e352ad100b100a0b628d20daff4916b2606493e8
Ahmed <lunitmohammed697@gmail.com>
2024-11-20 20:10:35 +0100

    update

--- mproject/mapp/static/script.js
+++ mproject/mapp/static/script.js
@@ -5,3 +5,9 @@ document.getElementById('import-btn').addEventListener('click', function() {
 document.getElementById('file-input').addEventListener('change', function() {
     console.log(this.files[0]); // Log the selected file for demonstration
 });
+
+
+document.getElementById('id_profile_picture').addEventListener('change', function(event) { const [file] = event.target.files; 
+    if (file) { const reader = new FileReader(); reader.onload = function(e) { const img = document.getElementById('profile-picture-preview');
+         img.src = e.target.result; img.style.display = 'block';} 
+         reader.readAsDataURL(file); } });
