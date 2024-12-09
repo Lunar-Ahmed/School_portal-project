3726ed1ea2e5195c9f1bbedfc1ff0866b4aed2a5
Ahmed <lunitmohammed697@gmail.com>
2024-12-09 13:27:05 +0100

    updates

--- mproject/mapp/views.py
+++ mproject/mapp/views.py
@@ -19,35 +19,6 @@ def admin(request):
     template = loader.get_template('base.html')
     return HttpResponse(template.render())
 
-# def teacher(request):
-#     teachd = Teacher.objects.all().values()
-#     template = loader.get_template('teacherboard.html')
-#     context = {
-#         'teachd': teachd,
-#     }
-#     return HttpResponse(template.render(context, request)
-# def teacher(request, Username):
-#     teachd = Teacher.objects.get(Username=Username)
-#     template = loader.get_template('teacherboard.html')
-#     context = {
-#         'teachd': teachd,
-#     }
-#     return HttpResponse(template.render(context, request))
-
-
-# def teacher(request):
-#     teacher_data = Teacher.objects.all()
-#     template = loader.get_template('teacherboard.html')
-#     context = {
-#         'teacher_data': teacher_data,
-#     }
-#     return HttpResponse(template.render(context, request))
-
-
-# def teacher(request):
-#     template = loader.get_template('teacherboard.html')
-#     return HttpResponse(template.render())
-
 
 def student(request):
     template = loader.get_template('studentboard.html')
@@ -178,52 +149,8 @@ def student_scores(request):
 
 
 
-# def student_register(request):
-#     form = StudentRegForm()
-#     if request.method == 'POST':
-#         form = StudentRegForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             return redirect('student_login')
-#     # else:
-#     #     form = UserRegistrationForm()
-#     return render(request, 'student_register.html', {'form': form})
 
 
-# def authority_login(request):
-#     form = AuthorityLogForm()
-#     if request.method == 'POST':
-#         form = AuthorityLogForm(request.POST)
-#         if form.is_valid():
-#             email = form.cleaned_data['email']
-#             department = form.cleaned_data['department']
-#             password = form.cleaned_data['password']
-#             user = authenticate(request, email=email, password=password)
-#             if user is not None:
-#                 login(request, user)
-#                 if department == 'A':
-#                     return redirect('admin')
-#                 elif department == 'T':
-#                     return redirect('teacher')
-#                 elif department == 'B':
-#                     return redirect('bursar')
-#             # else:
-#             #     form.add_error(None, 'Invalid email, department, or password')
-#     # context = {'loginform': form}
-#     return render(request, 'authority_login.html',{'form': form}) #context=context)
-
-
-# def admin_register(request):
-#     form = AdminRegForm()
-#     if request.method == 'POST':
-#         form = Reg(request.POST)
-#         if form.is_valid(): 
-#             form.save()
-#             return redirect('login')
-#     context = {'registerform': form}
-#     # else:
-#     #     form = RegistrationForm()
-#     return render(request, 'regform.html', context=context)# {'context': form})
 
 
 
@@ -235,59 +162,6 @@ def student_scores(request):
 def student_login(request):
     template = loader.get_template('student_login.html')
     return HttpResponse(template.render())
-
-# def admin_login(request):
-#     if request.method == 'POST':
-#         form = AdminLogin(request.POST)
-#         if form.is_valid():
-#             username = form.cleaned_data['username']
-#             password = form.cleaned_data['password']
-#         admin_log = admin.objects.filter(username=username, password=password).first()
-#         if admin_log:
-#             request.session['role'] = 'Admin'
-#             request.session['user_id'] = admin_log.id
-#             return redirect('admin')
-#         else:
-#             form = AdminLogin()
-#             return render(request, 'admin_log.html', {'form': form})
-# def admin_login(request):
-#     if request.method == 'POST':
-#         form = AdminLogin(request.POST)
-#         if form.is_valid():
-#             username = form.cleaned_data['username']
-#             password = form.cleaned_data['password']
-#             admin_log = Admin.objects.filter(username=username, password=password).first()
-#             if admin_log:
-#                 request.session['role'] = 'Admin'
-#                 request.session['user_id'] = admin_log.id
-#                 return redirect('admin')
-#         else:
-#             form.add_error(None, 'Invalid username or password') 
-#     else:
-#         form = AdminLogin()
-    
-#     return render(request, 'admin_log.html', {'form': form})
-
-
-
-# def authority_login(request):
-#     form = AuthorityLogForm()
-#     if request.method == 'POST':
-#         form = AuthorityLogForm(request.POST)
-#         if form.is_valid():
-#             username = form.cleaned_data['username']
-#             password = form.cleaned_data['password']
-#             user = authenticate(request, username=username, password=password)
-#             if user is not None:
-#                 login(request, user)
-#                 return redirect('home')  # Redirect to a success page
-#             else:
-#                 form.add_error(None, 'Invalid username or password')
-#     context = {'loginform': form}
-#     return render(request, 'login.html', context=context)
-
-
-
 
 
 # views.py
@@ -306,7 +180,7 @@ def toggle_teacher_status(request, teacher_id):
         teacher.user.is_active = not teacher.is_disabled
         teacher.user.save()
 
-    return redirect('acad')  # Redirect back to the teacher list page
+    return redirect('acad', request)  # Redirect back to the teacher list page
 
 
 
